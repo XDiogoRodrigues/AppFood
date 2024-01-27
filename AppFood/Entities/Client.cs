73fe078b9ce92a69c23d494ffba481fe5987b29b
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-27 13:56:21 -0300

    Iniciado o projeto. declarada a classe Client com seus atributos e metodo construtor

---
+++ AppFood/Entities/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFood.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public Client(string name, string phone, string address)
+        {
+            Name = name;
+            Phone = phone;
+            Address = address;
+        }
+    }
+}
