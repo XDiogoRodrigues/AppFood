5fa25552f19943acd76121cfa7b04a86d8654aa8
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-27 20:29:25 -0300

    Declarada a views que lista todos os refrigerantes

---
+++ AppFood/Views/ShowRefrigerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+
+namespace AppFood.Views
+{
+    class ShowRefrigerator
+    {
+        public static void Show()
+        {
+            Dictionary<string, double> dict = new Dictionary<string, double>();
+            string path = @"C:\Users\diogo\Desktop\MeusProjetos\AppFood\Products\Refrigerator\Refrigerator.txt";
+
+            using(StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] vect = sr.ReadLine().Split(',');
+                    string nameRefrigerator = vect[0];
+                    double priceRefrigerator = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                    dict.Add(nameRefrigerator, priceRefrigerator);
+                }
+            }
+
+            int number = 1;
+            foreach(var refri in dict)
+            {
+                Console.WriteLine($"{number}-) Refrigerator name: {refri.Key}, Price: R$ {refri.Value.ToString("F2", CultureInfo.InvariantCulture)}");
+                number++;
+            }
+        }
+    }
+}
--- AppFood/Views/ShowSnacks.cs
+++ AppFood/Views/ShowSnacks.cs
@@ -5,9 +5,9 @@ using System.Globalization;
 
 namespace AppFood.Views
 {
-    class ShowProducts
+    class ShowSnacks
     {
-        public static void ShowSnacks()
+        public static void Show()
         {
             string path = @"C:\Users\diogo\Desktop\MeusProjetos\AppFood\Products\Snacks\snacks.txt";
             Dictionary<string, double> dict = new Dictionary<string, double>();
@@ -15,7 +15,7 @@ namespace AppFood.Views
             using (StreamReader s1 = File.OpenText(path)) {
                 while (!s1.EndOfStream)
                 {
-                    string[] vect = s1.ReadLine().Split(',');
+                    string[] vect = s1.ReadLine().Split('_');
                     string nameSnack = vect[0];
                     double priceSnack = double.Parse(vect[1], CultureInfo.InvariantCulture);
                     dict.Add(nameSnack, priceSnack);
