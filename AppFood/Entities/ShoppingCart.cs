205a62729268acb28fe716ce3c833b1e9cde64ac
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-27 14:16:58 -0300

    Declarada a classe estatica ShoppingCart e a classe Product com seus atributos e metodo construtor

---
+++ AppFood/Entities/ShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFood.Entities
+{
+    static class ShoppingCart
+    {
+        static public List<Product> products = new List<Product>();
+
+    }
+}
