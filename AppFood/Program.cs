622a94ce03a07f9f51babf1f2a6bff628c9ba627
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-28 19:24:36 -0300

    Começando a implementar o programa principal. classe ChoiceScreen declarada com seu metodo

--- AppFood/Program.cs
+++ AppFood/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppFood.Entities;
+using AppFood.Views;
 
 
 namespace AppFood
@@ -11,6 +13,17 @@ namespace AppFood
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("What would you like to accomplish: (1- Login | 2- Register)\nChoice: ");
+                int choice = int.Parse(Console.ReadLine());
+                ChoiceScreen.ChoicesScreen(choice);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Option invalid or invalid data!");
+            }
+           
         }
     }
 }
---
+++ AppFood/Views/ChoiceScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFood.Views
+{
+    class ChoiceScreen
+    {
+        public static void ChoicesScreen(int choice)
+        {
+            if(choice == 1)
+            {
+                Console.WriteLine("-------------- Login ------------------");
+
+            }
+            else if(choice == 2)
+            {
+                Console.WriteLine("-------------- Register ---------------");
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida!");
+            }
+        }
+    }
+}
